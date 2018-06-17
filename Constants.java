ad0e6128a50b8eb09ae6c3d1afefa30e49025a75
Lucknell <Lucknell@email.com>
2018-06-16 23:04:40 -0400

    Create Constants.java

---
+++ Constants.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author lucknell
+ */
+public final class Constants {
+    
+    public static final char LEFT_NORMAL = '(';
+    public static final char RIGHT_NORMAL = ')';
+    public static final char LEFT_BRACKET = '[';
+    public static final char RIGHT_BRACKET = ']';
+    public static final char LEFT_BRACE = '{';
+    public static final char RIGHT_BRACE = '}';
+    public static final char PLUS = '+';
+    public static final char MINUS = '-';
+    public static final char MULTIPLY = '*';
+    public static final char DIVIDE = '/';
+    
+    
+}
